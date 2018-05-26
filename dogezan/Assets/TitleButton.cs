26a3428d4982f10efb2dd2f8b9c99270e3a4f79e
Kenta Yamaki <drgarugari@gmail.com>
2018-05-26 15:49:26 +0900

    タイトル更新

---
+++ dogezan/Assets/TitleButton.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour {
+	public float Step = 0.05f;
+	float alpha = 0.0f;
+
+	// Use this for initialization
+	void Awake () {
+		enabled = false;
+		alpha = 0.0f;
+		SetAlpha (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!enabled)
+			return;
+		
+		alpha += Step;
+		if (alpha < 0 || alpha > 1) {
+			Step = Step * -1;
+			alpha += Step;
+		}
+		SetAlpha (alpha);
+
+
+		if (Input.anyKeyDown) {
+			AudioPlay ("Audio Source3");
+			Invoke ("ChangeScene", 2);
+		}
+	}
+
+	void ChangeScene()
+	{	
+		SceneManager.LoadScene ("Game");
+	}
+
+	void SetAlpha(float a)
+	{
+		var t = GameObject.Find ("ShobuText").GetComponent<Text> ();
+		var tc = t.color;
+		tc.a = a;
+		t.color = tc;
+
+		var ts = GameObject.Find("ShobuTextShadow").GetComponent<Text>();
+		var tsc = ts.color;
+		tsc.a = a * (165.0f / 256.0f);
+		ts.color = tsc;
+		
+	}
+
+	void AudioPlay(string name){
+		var go = GameObject.Find (name);
+		if (go != null) {
+			var src = go.GetComponent<AudioSource> ();
+			if (src != null) {
+				src.Play ();
+			}
+		}
+	}
+}
--- dogezan/Assets/TitleCutMove01.cs
+++ dogezan/Assets/TitleCutMove01.cs
@@ -13,10 +13,15 @@ public class TitleCutMove01 : MonoBehaviour {
 	public iTween.EaseType EaseType2=iTween.EaseType.linear;
 	public float PositionY2 = -200;
 
+	public bool PlaySE = false;
+
 //	public float Dela
 
 	// Use this for initialization
 	void Start () {
+		var button = GameObject.Find ("Button").GetComponent<TitleButton> ();
+		button.enabled = false;
+
 		var image = this.gameObject.GetComponent<UnityEngine.UI.Image> ();
 		image.enabled = true;
 		iTween.MoveTo (this.gameObject, iTween.Hash (
@@ -26,8 +31,12 @@ public class TitleCutMove01 : MonoBehaviour {
 			"easeType", EaseType1,
 			"delay", Delay1,
 			"time", Time1,
-			"oncomplete", "OncompleteHandler"
+			"oncomplete", "OncompleteHandler",
+			"onstart", "OnstartHandler"
 		));
+	}
+	void OnstartHandler() {
+		AudioPlay ("Audio Source1");
 	}
 
 	void OncompleteHandler() {
@@ -38,9 +47,36 @@ public class TitleCutMove01 : MonoBehaviour {
 			"easeType", EaseType2,
 			"delay", Delay2,
 			"time", Time2,
-			"oncomplete", "OncompleteHandler"
+			"oncomplete", "OncompleteHandler2",
+			"onstart", "OnstartHandler2"
 		));
 	}
+
+	void OnstartHandler2() {
+		AudioPlay ("Audio Source2");
+	}
+	void OncompleteHandler2(){
+		Invoke ("ButtonVisible", 2);
+	}
+	void ButtonVisible() {
+		if (PlaySE) {
+			var button = GameObject.Find ("Button").GetComponent<TitleButton> ();
+			button.enabled = true;
+		}
+	}
+
+	void AudioPlay(string name){
+		if (PlaySE) {
+			var go = GameObject.Find (name);
+			if (go != null) {
+				var src = go.GetComponent<AudioSource> ();
+				if (src != null) {
+					src.Play ();
+				}
+			}
+		}
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
