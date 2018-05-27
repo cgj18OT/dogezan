b79edceaa4d1dc57ef36705238ee7b7509d06d95
Kenta Yamaki <drgarugari@gmail.com>
2018-05-27 11:46:57 +0900

    タイトルとリザルト修正

---
+++ dogezan/Assets/DelayAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DelayAudio : MonoBehaviour {
+	public GameObject AudioObject;
+
+	public float Delay = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Play", Delay);
+	}
+	void Play(){
+		AudioObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ dogezan/Assets/ResultManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResultManager : MonoBehaviour {
+	public Text ResultText;
+	public GameObject FadeObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKeyDown) {
+			FadeObject.SetActive (true);
+			Invoke ("NextScene", 2);
+		}
+	}
+
+	void NextScene()
+	{
+		SceneManager.LoadScene ("Title");
+	}
+}
